90a887b3d6550b86ac7118ed6ec9c53aff346b5b
Aman <ak669212@gmail.com>
2024-08-25 17:46:42 +0530

    changes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,41 +2,49 @@
 
 @section('content')
     <section class="home_page px-3 d-flex flex-wrap">
-        <div class="card mb-3 w-50">
-            <a href={{ route("lot.index") }}>
-                <div class="card-body">
-                    <h5 class="card-title mb-4">Lot</h5>
-                    {{-- <a href={{ route('lot.add') }} class="btn btn-success btn-sm">Add</a> --}}
-                    {{-- <a href={{ route("lot.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
-                </div>
-            </a>
+        <div class="w-50 px-1">
+            <div class="card mb-3">
+                <a href={{ route("lot.index") }}>
+                    <div class="card-body">
+                        <h5 class="card-title mb-4">Lot</h5>
+                        {{-- <a href={{ route('lot.add') }} class="btn btn-success btn-sm">Add</a> --}}
+                        {{-- <a href={{ route("lot.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
+                    </div>
+                </a>
+            </div>
         </div>
-        <div class="card mb-3 w-50">
-            <a href={{ route("party.index") }}>
-                <div class="card-body">
-                    <h5 class="card-title mb-4">Party</h5>
-                    {{-- <a href={{ route('party.add') }} class="btn btn-success btn-sm">Add</a> --}}
-                    {{-- <a href={{ route("party.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
-                </div>
-            </a>
+        <div class="w-50 px-1">
+            <div class="card mb-3">
+                <a href={{ route("party.index") }}>
+                    <div class="card-body">
+                        <h5 class="card-title mb-4">Party</h5>
+                        {{-- <a href={{ route('party.add') }} class="btn btn-success btn-sm">Add</a> --}}
+                        {{-- <a href={{ route("party.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
+                    </div>
+                </a>
+            </div>
         </div>
-        <div class="card mb-3 w-50">
-            <a href={{ route("worker.index") }}>
-                <div class="card-body">
-                    <h5 class="card-title mb-4">Worker</h5>
-                    {{-- <a href={{ route('worker.add') }} class="btn btn-success btn-sm">Add</a> --}}
-                    {{-- <a href={{ route("lot.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
-                </div>
-            </a>
+        <div class="w-50 px-1">
+            <div class="card mb-3">
+                <a href={{ route("worker.index") }}>
+                    <div class="card-body">
+                        <h5 class="card-title mb-4">Worker</h5>
+                        {{-- <a href={{ route('worker.add') }} class="btn btn-success btn-sm">Add</a> --}}
+                        {{-- <a href={{ route("lot.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
+                    </div>
+                </a>
+            </div>
         </div>
-        <div class="card mb-3 w-50">
-            <a href={{ route("workerType.index") }}>
-                <div class="card-body">
-                    <h5 class="card-title mb-4">Worker Type</h5>
-                    {{-- <a href={{ route('workerType.add') }} class="btn btn-success btn-sm">Add</a> --}}
-                    {{-- <a href={{ route("lot.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
-                </div>
-            </a>
+        <div class="w-50 px-1">
+            <div class="card mb-3">
+                <a href={{ route("workerType.index") }}>
+                    <div class="card-body">
+                        <h5 class="card-title mb-4">Worker Type</h5>
+                        {{-- <a href={{ route('workerType.add') }} class="btn btn-success btn-sm">Add</a> --}}
+                        {{-- <a href={{ route("lot.index") }} class="btn btn-primary btn-sm">Listing</a> --}}
+                    </div>
+                </a>
+            </div>
         </div>
     </section>
 @endsection
